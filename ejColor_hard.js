0a5ab85f88edf71b15c91014789276202ce0bbfe
Jairo3000 <119258337+jairocod3000@users.noreply.github.com>
2023-10-27 12:09:06 +0200

    Add files via upload

---
+++ ejColor_hard.js
@@ -0,0 +1,48 @@
+const colors = generateRandomColors(6);
+const colorDisplay = document.getElementById("color-display");
+const colorBoxes = document.querySelectorAll(".color-box");
+const mensaje = document.getElementById("mensaje");
+
+let pickedColor = pickColor();
+
+colorDisplay.textContent = pickedColor;
+
+for (let i = 0; i < colorBoxes.length; i++) {
+    colorBoxes[i].style.backgroundColor = colors[i];
+
+    colorBoxes[i].addEventListener("click", function() {
+        if (this.style.backgroundColor === pickedColor) {
+            mensaje.textContent = "¡Correcto!";
+            changeColors(pickedColor);
+        } else {
+            this.style.backgroundColor = "#232323";
+            mensaje.textContent = "Inténtalo de nuevo";
+        }
+    });
+}
+
+function generateRandomColors(num) {
+    const arr = [];
+    for (let i = 0; i < num; i++) {
+        arr.push(randomColor());
+    }
+    return arr;
+}
+
+function randomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+function pickColor() {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+}
+
+function changeColors(color) {
+    for (let i = 0; i < colorBoxes.length; i++) {
+        colorBoxes[i].style.backgroundColor = color;
+    }
+}
